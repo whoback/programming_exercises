2550c99e29c3df5c909a9bcba3256418f138408c
Will Hoback <willhoback@Will-Hobacks-MacBook-Pro.local>
2018-07-31 09:48:47 -0500

    refactor to only accept specific inputs and reprompt user

--- temp_converter.py
+++ temp_converter.py
@@ -23,16 +23,25 @@
 # updates the values when any value changes.
 # • Modify the program so it also supports the Kelvin scale.
 
+cel_strings = ['c', 'C']
+far_strings = ['f', 'F']
+
+def conversion_choice():
+	global conversion_choice
+	
+	while conversion_choice not in cel_strings and conversion_choice not in far_strings:
+		conversion_choice = str(input('Press C to convert from Fahrenheit to Celsius. \n''Press F to convertfrom Celsius to Fahrenheit. \n''Your choice: '))
+
+	return(conversion_choice)
+
 def to_fahrenheit():
 	temp_in_celcius = int(input('Please enter the temperature in Celsius: '))
 	converted_to_fahrenheit = (temp_in_celcius * 9 / 5) + 32
-	# print('The temperature in Fahrenheit is: ' + str(converted_to_fahrenheit))
 	print_results('Fahrenheit', converted_to_fahrenheit)
 
 def to_celcius():
 	temp_in_fahrenheit = int(input('Please enter the temperature in Fahrenheit: '))
 	converted_to_celcius = (temp_in_fahrenheit - 32) * 5 / 9
-	# print('The temperature in Celsius is: ' + str(converted_to_celcius))
 	print_results('Celcius', converted_to_celcius)
 
 
@@ -44,15 +53,12 @@ def which_key(conversion_choice):
 	if conversion_choice in cel_strings:
 		to_celcius()
 	elif conversion_choice in far_strings:
-		to_fahrenheit()
-	else:
-		print('neither')
+		to_fahrenheit()			
 
 
-cel_strings = ['c', 'C']
-far_strings = ['f', 'F']
 
-conversion_choice = str(input('Press C to convert from Fahrenheit to Celsius. \n''Press F to convertfrom Celsius to Fahrenheit. \n''Your choice: '))
+
+conversion_choice()
 
 which_key(conversion_choice)
 
