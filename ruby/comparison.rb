f732592c1dd0f5fedfa41260e23e4da33ad22f5d
Will Hoback <willhoback@Will-Hobacks-MacBook-Pro.local>
2018-08-01 17:13:25 -0500

    comparisons

---
+++ ruby/comparison.rb
@@ -0,0 +1,49 @@
+# 6 comparison expressions each of the operations: ==, !=, <, >, <=, >=. 
+
+puts "Equal to"
+puts 4 == 4
+puts 2 == 1
+puts "a" == "b"
+puts true == 4
+puts true == true
+puts false == 0
+
+puts "Not equal to"
+puts true != false
+puts false != false
+puts 100 != -100
+puts 0 != 0
+puts 0 != "zero"
+puts "a" != "b"
+
+puts "less than"
+puts 4 < 4
+puts 2 < 1
+puts "a" < "b"
+puts -4 < 4
+puts 'orange' < 'tree'
+puts 0 < 0
+
+puts "greater than"
+puts "true" > "true"
+puts "false" > "true"
+puts 100 > -100
+puts 0 > 0
+puts 0 > -1
+puts "a" > "b"
+
+puts "less than or equal to"
+puts "true" <= "true"
+puts "false" <= "true"
+puts 100 <= -100
+puts 0 <= 0
+puts 0 <= -1
+puts "a" <= "b"
+
+puts "greater than or equal to"
+puts "true" >= "true"
+puts "false" >= "true"
+puts 100 >= -100
+puts 0 >= 0
+puts 0 >= -1
+puts "a" >= "b"
