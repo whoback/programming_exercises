3c0fd2f38a20edb7514129621e2204c3e01920ab
Will Hoback <willhoback@Will-Hobacks-MacBook-Pro.local>
2018-08-01 16:58:43 -0500

    simple math examples

---
+++ ruby/simple_math.rb
@@ -0,0 +1,41 @@
+# 4 examples of +, -, /, % operations. use puts to output each.
+
+puts 'Addition'
+a1 = 5 + 5
+a2 = 72.5 + 0.01
+a3 = -33 + 3
+a4 = 0 + 0
+puts "5 + 5 = #{a1} "
+puts "72.5 + 0.01 = #{a2}"
+puts "-33 + 3 = #{a3}"
+puts "0 + 0 = #{a4}"
+
+puts 'Subtraction'
+s1 = 17 - 4
+s2 = 4 - 0.75
+s3 = -5 - 5
+s4 = 1000 - 4340
+puts "17 - 4 = #{s1}"
+puts "4 - 0.75 = #{s2}"
+puts "-5 - 5 = #{s3}"
+puts "1000 - 4340 = #{s4}"
+
+puts 'Division'
+d1 = 40 / 4
+d2 = 0 / 3
+d3 = -10 / 5
+d4 = 75 / 100
+puts "40 / 4 = #{d1}"
+puts "0 / 3 = #{d2}"
+puts "-10 / 5 = #{d3}"
+puts "75 / 100 = #{d4}"
+
+puts 'Modulo'
+m1 = 2 % 1
+m2 = 4 % 0.75
+m3 = -5 % 5
+m4 = 1000 % 4340
+puts "2 % 1 = #{s1}"
+puts "4 % 0.75 = #{s2}"
+puts "-5 % 5 = #{s3}"
+puts "1000 % 4340 = #{s4}"
