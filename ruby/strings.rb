721d98511ae8676fdf32fa374bce37f47177d6e5
Will Hoback <willhoback@Will-Hobacks-MacBook-Pro.local>
2018-08-01 17:32:57 -0500

    strings

---
+++ ruby/strings.rb
@@ -0,0 +1,15 @@
+# Write 3 expressions using each of string operations: + (concatenation), .length, and [] (indexing) Print out the result of your expressions using puts. 
+puts "My" + "string"
+puts "My " + "string " + "has " + "spaces!"
+puts "Give me liberty... " + "or give me DEATH!"
+
+a = "My" + "string"
+b = "My " + "string " + "has " + "spaces!"
+c = "Give me liberty... " + "or give me DEATH!"
+puts a.length
+puts b.length
+puts c.length
+
+puts a[4]
+puts b[3..7]
+puts c[14 .. 40]
