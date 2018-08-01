f59b9deaa9e826f69cf0b72b5171af069a545bbf
Will Hoback <willhoback@Will-Hobacks-MacBook-Pro.local>
2018-08-01 17:22:48 -0500

    add booleans

---
+++ ruby/booleans.rb
@@ -0,0 +1,19 @@
+# Write 4 examples or expressions using each of the operations: &&, ||, !
+
+puts "And"
+puts true && true
+puts true && false
+puts false && true
+puts false && false
+
+puts "Or"
+puts true || true
+puts true || false
+puts false || true
+puts false || false
+
+puts "Not"
+puts true && !true
+puts true && !false
+puts false && !true
+puts false && !false
