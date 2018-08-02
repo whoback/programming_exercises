0b2eaa2e532a4c97cc5c50c09d4c1636446090a1
Will Hoback <willhoback@Will-Hobacks-MacBook-Pro.local>
2018-08-02 05:58:07 -0500

    variables

---
+++ ruby/variables.rb
@@ -0,0 +1,16 @@
+# 1. Declare a variable named `firstName` and assign it your first name as a string:
+
+firstName = 'Will'
+
+# 2. Declare a variable named `lastName` and assign it your last name as a string:
+
+lastName = 'Hoback'
+
+# 3. Declare a variable named `fullName` and assign it the concatenation of
+# your `firstName` and `lastName`. Be sure to put a space between your
+# first and last names:
+
+fullName = firstName + ' ' + lastName
+
+# 4. print out the `fullName` variable
+puts fullName
